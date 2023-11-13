4f99b77ad23f299192adcf138a5339dfbcb847b9
HebaZaki <h_borhan@hotmail.com>
2023-11-13 15:22:40 +0200

    functions

---
+++ functions.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @string: the string to be checked
+ *
+ * Return: i which is the length of the string
+ */
+
+int _strlen(char *string)
+{
+	int length;
+
+	for (length = 0; string[length] != '\0'; length++)
+	{
+	}
+
+	return (length);
+}
