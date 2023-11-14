6dd1909e64ae9b2426455f3a42b842d5e2042b99
HebaZaki <h_borhan@hotmail.com>
2023-11-14 14:29:24 +0200

    _printf: task 0

--- task0.c
+++ task0.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _printf - Custom printf func
+ * _printf - Custom printf function
  * @format: Format specifier string
  * Return: Number of char printed excluding null byte
  */
@@ -8,42 +8,56 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, length;
-
-	if (format == NULL)
+	va_start(args, format);
+	
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 	{
 		return (-1);
 	}
-	va_start(args, format);
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
+
 		if (format[i] != '%')
 		{
 			char a = format[i];
 
 			write(1, &a, 1);
-		} else if (format[i] == '%' && format[i + 1] == '%')
+		}
+		else if (format[i] == '%')
 		{
-			char b = format[i];
-
-			write(1, &b, 1);
 			i++;
-		} else if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			char c = va_arg(args, int);
-
-			write(1, &c, 1);
-			i++;
-		} else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			char *string = va_arg(args, char *);
-
-			if (string == NULL)
+			while (format[i] == ' ')
 			{
-				write(1, "(null)", 6);
-			} else
-				length = _strlen(string);
-			write(1, string, length);
-			i++;
+				i++;
+			}
+			if (format[i] == '%')
+			{
+				char b = format[i];
+				write(1, &b, 1);
+			}
+			else if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+			}
+			else if (format[i] == 's')
+			{
+				char *string = va_arg(args, char *);
+
+				if (string == NULL)
+				{
+					write(1, "(null)", 6);
+				}
+				else if (string == 0)
+				{
+					break;
+				}
+				else
+					length = _strlen(string);
+					write(1, string, length);
+			}
 		}
 	}
 	va_end(args);
