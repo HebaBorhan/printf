76ebab792bc215fe7803ddb9775fc7ef9090e690
“HebaM94” <hebamagde@gmail.com>
2023-11-14 16:30:50 -0800

    update

--- print_integer.c
+++ print_integer.c
@@ -1,19 +1,17 @@
-#include "main.h"
-#include <stddef.h>
+#include <stdarg.h>
 #include <unistd.h>
-
+#include "main.h"
 /**
 * print_int - function that handels conversion specifiers
 * of d and i specifiers
 * @integers: list of arguments to be prented
 * Return: counter
 */
-int print_int(va_list integers)
+int print_int(va_list args)
 {
-int num = va_arg(integers, int);
+int num = va_arg(args, int);
 int counter, decimal, digit;
-char sign = '\0';
-char integer;
+char integer, sign = '\0';
 decimal = 1;
 counter = 0;
 if (num < 0)
@@ -23,21 +21,18 @@ write(1, &sign, 1);
 num = -num;
 counter++;
 }
-if (num > 0)
-{
 while ((num / decimal) >= 10)
 {
 decimal = decimal * 10;
 }
-while (decimal > 0)
+while (decimal >= 1)
 {
 digit = num / decimal;
 integer = digit + '0';
 write(1, &integer, 1);
-num = num  % decimal;
+num = num - (decimal * 10);
 decimal = decimal / 10;
 counter++;
-}
 }
 return (counter);
 }
