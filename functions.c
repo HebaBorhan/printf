3ab71dd5efb145aa61e4bad3332fb302a033393f
HebaZaki <h_borhan@hotmail.com>
2023-11-15 20:34:12 +0200

    C - printf

--- functions.c
+++ functions.c
@@ -1,57 +1,55 @@
 #include "main.h"
 #include <stddef.h>
 #include <unistd.h>
+
 /**
-* _strlen - returns the length of a string
-* @string: the string to be checked
-* Return: i which is the length of the string
-*/
+ * _strlen - returns the length of a string
+ * @string: the string to be checked
+ *
+ * Return: i which is the length of the string
+ */
+
 int _strlen(char *string)
 {
-int length;
-for (length = 0; string[length] != '\0'; length++)
-{
+    int length;
+
+    for (length = 0; string[length] != '\0'; length++)
+    {
+    }
+
+    return (length);
 }
-return (length);
-}
-/**
-* print_char - prints character
-* @args: arguments
-* Return: 1 which is char length
-*/
+
 int print_char(va_list args)
 {
-char c = va_arg(args, int);
-write(1, &c, 1);
-return (1);
+    char c = va_arg(args, int);
+
+    write(1, &c, 1);
+    return (1);
 }
-/**
-* print_string - prints string
-* @args: arguments
-* Return: string length
-*/
+
 int print_string(va_list args)
 {
-char *string = va_arg(args, char *);
-int length;
-length = 0;
+    char *string = va_arg(args, char *);
+    int length = 0;
 
-if (string == NULL)
-{
-write(1, "(null)", 6);
-return (6);
+    if (string == NULL)
+    {
+        write(1, "(null)", 6);
+        return (6);
+    }
+    else
+    {
+        length = _strlen(string);
+        write(1, string, length);
+        length++;
+        return (length);
+    }
 }
-length = _strlen(string);
-write(1, string, length);
-return (length);
-}
-/**
-* print_per - prints %
-* Return: 1 which is % length
-*/
+
 int print_per(void)
 {
-char b = '%';
-write(1, &b, 1);
-return (1);
+    char b = '%';
+    write(1, &b, 1);
+    return (1);
 }
--- main.h
+++ main.h
@@ -2,19 +2,20 @@
 #define MAIN_H
 #include <stdarg.h>
 /**
-* struct spec - Struct of specifiers
-* @spec: specifiers
-* @f: function associated
-*/
+ * struct spec - Struct of specifiers
+ * @spec: specifiers
+ * @f: function associated
+ */
 typedef struct spec
 {
-char *spec;
-int (*f)();
+    char spec;
+    int (*f)();
 } spec_t;
 int _strlen(char *string);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_per(void);
 int print_int(va_list integers);
+int func(const char *format, va_list args);
 int _printf(const char *format, ...);
 #endif
