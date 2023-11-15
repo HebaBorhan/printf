07db62259b7e9555bdfbc9e012da98975e3e76af
“HebaM94” <hebamagde@gmail.com>
2023-11-15 15:53:09 -0800

    update

--- func.c
+++ func.c
@@ -2,49 +2,51 @@
 #include <stddef.h>
 #include <unistd.h>
 /**
- * func - dealing with struct and format
- * @format: string
- * @args: arguments
- *
- * Return: count
- */
+* func - dealing with struct and format
+* @format: string
+* @args: arguments
+* Return: count
+*/
 int func(const char *format, va_list args)
 {
 spec_t sps[] = {
-{'c', print_char},
-{'s', print_string},
-{'%', print_per},
-{'\0', NULL}};
-int i, j, count = 0;
-
+{'c', print_char}, {'s', print_string}, {'%', print_per}};
+int i, j, temp, count = 0;
 for (i = 0; format[i] && format; i++)
 {
+Start:
 if (format[i] != '%')
 {
 char a = format[i];
-
 count += write(1, &a, 1);
 }
 else if (format[i] == '%')
 {
-while (format[i + 1] == ' ')
-{
 i++;
+temp = i;
+while (format[i] == ' ')
+{
+temp++;
 }
-if (format[i + 1] == '\0')
+if (format[i] == '\0' || format[temp] == '\0')
 {
 return (-1);
 }
 else
 {
-for (j = 0; sps[j].spec != '\0'; j++)
+while (j >= 0)
 {
-if (format[i + 1] == sps[j].spec)
+if (format[i] == sps[j].spec)
 {
 count += sps[j].f(args);
-i += 1;
+i++;
+goto Start;
 }
+j--;
 }
+char b = format[i];
+count += write(1, &b, 1);
+i++;
 }
 }
 }
