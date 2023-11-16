59ce28b4e33b42f9e235d3930dd6945c59d954ee
HebaZaki <h_borhan@hotmail.com>
2023-11-16 09:01:37 +0200

    helper functions

--- func.c
+++ func.c
@@ -31,11 +31,6 @@ while (format[i + 1] == ' ')
 {
 i++;
 }
-if (format[i + 1] == '%')
-{
-count = print_per();
-i += 1;
-}
 for (j = 0; sps[j].spec != '\0'; j++)
 {
 if (format[i + 1] == sps[j].spec)
