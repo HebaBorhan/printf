32927cc14b61dca535961bd1830e75a31c073e6c
HebaZaki <h_borhan@hotmail.com>
2023-11-17 21:11:06 +0200

    handling functions

--- handler.c
+++ handler.c
@@ -10,15 +10,15 @@
 int handler(char s, va_list args)
 {
 int i, count = 0;
-spec_t sps[] = {{'c', print_char}, {'s', print_string}, {'\0', NULL}};
-for (i = 0; i <= 3; i++)
+spec_t sps[] = {{'c', print_char}, {'s', print_string}, {'r', print_rev}, {'\0', NULL}};
+for (i = 0; i <= 4; i++)
 {
 if (s == sps[i].spec)
 {
 count += sps[i].f(args);
 break;
 }
-else if (i == 3 && s != sps[i].spec)
+else if (i == 4 && s != sps[i].spec)
 {
 count += 2;
 write(1, "%", 1);
